6adac4904767f0ad55f41608c98e9e592087a8ab
Bruno <skape__@hotmail.com>
2019-04-21 08:08:29 -0300

    commit inicial

---
+++ src/main/java/br/com/exemplo/testes/API_RestAssured/App.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.testes.API_RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/exemplo/testes/api/RestAssured_APITest.java
@@ -0,0 +1,58 @@
+package br.com.exemplo.testes.api;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+
+public class RestAssured_APITest
+{
+	@BeforeClass
+	public static void setUp()
+	{
+		RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+	}
+	
+	@Test
+	public void getRequest()
+	{
+		given().
+		when().
+			get("/posts/1").
+		then().
+			statusCode(200).
+			body("userId", is(1)).
+			body("id", is(1)).
+			body("title", containsString("sunt aut facere"));
+	}
+	
+	@Test
+	public void postRequest()
+	{
+		given().
+			contentType("application/json").
+			body("{ \"title\":\"VR Dev Summit\", \"body\":\"blabla\", \"userId\":123 }").
+		when().
+			post("posts").
+		then().
+			statusCode(201).
+			body("title", containsString("VR Dev Summit")).
+			body("body", containsString("blabla")).
+			body("userId", is(123));
+	}
+	
+	@Test
+	public void endpointNotFound()
+	{
+		given().
+			contentType("application/json").
+		when().
+			post("/naoexiste").
+		then().
+			statusCode(404);
+	}
+}
